76fc5cd2af360bfc1ec88de7271593d8ab2ab938
henrfj <h1rik.f@gmail.com>
2022-01-19 21:15:33 +0100

    asd

--- square.c
+++ square.c
@@ -447,7 +447,7 @@ int main(){
 			// Obstacle 2 works
 
 			//cmd_followline(missions, bm, 0.15, drivendist, 1);
-			cmd_followline(missions, bl, 0.08, irdistfrontmiddle, 0.15);
+			cmd_followline(missions, bl, 0.05, irdistfrontmiddle, 0.15);
 
 			cmd_followline(missions, bm, 0.1, crossingblack, 0);
 			cmd_followline(missions, bm, 0.08, drivendist, 0.16);
